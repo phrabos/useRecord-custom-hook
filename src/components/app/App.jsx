5ebcf1519d62e263e7e1114e94c36fb249780cbb
patrick hrabos <phrabos@gmail.com>
2021-05-28 14:50:57 -0400

    undo, redo functionality working

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+const useRecord = (init) => {
+  const [current, setCurrent] = useState([init]);
+  const [idx, setIdx] = useState(0);
+
+  const record = (hex) => {
+    setCurrent((prev) => [...prev, hex]);
+    setIdx(current.length);
+  };
+ 
+  const undo = () => {
+    if(idx > 0) setIdx(prevIdx => prevIdx - 1);
+  };
+
+  const redo = () => {
+    if(idx < current.length - 1) setIdx(prevIdx => prevIdx + 1);
+  };
+
+
+  return { current, undo, redo, record, idx };
+};
+
+function App() {
+  const { current, undo, redo, record, idx } = useRecord('#FF0000');
+  
+  return (
+    <>
+      <button onClick={undo}>undo</button>
+      <button onClick={redo}>redo</button>
+      <input type="color" value={current[idx]} onChange={({ target }) => record(target.value)} />
+      <div style={{ backgroundColor: current[idx], width: '10rem', height: '10rem' }}></div>
+    </>
+  );
 }
+
+export default App;
